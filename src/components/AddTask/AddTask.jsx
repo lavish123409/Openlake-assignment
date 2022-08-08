8f68cc7daa7fbd06916c3bfbe7b73e2e992b614b
lavishg <lavishg@iitbhilai.ac.in>
2022-08-08 19:16:41 +0530

    corrected the path errors

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,44 +1,39 @@
-import React from 'react';
-import axios from 'axios';
-import './addtask.css';
+import React from "react";
+import axios from "axios";
+import "./addtask.css";
 
 function onSubmit() {
-  var inp = document.getElementById('taskMessage');
+  var inp = document.getElementById("taskMessage");
   // console.log(inp.value);
-  var url = 'https://open-lake-assignment.herokuapp.com/adddata';
+  const serverurl = process.env.REACT_APP_SERVER_URL;
 
-  axios.post(url , {mssg : inp.value})
-  .then(res => {
-    // console.log('Data added');
-    window.location.reload();
-    inp.value = '';
-  }).catch( err => console.log(err));
+  var url = serverurl + "/adddata";
 
-  
+  axios
+    .post(url, { mssg: inp.value })
+    .then((res) => {
+      // console.log('Data added');
+      window.location.reload();
+      inp.value = "";
+    })
+    .catch((err) => console.log(err));
 }
 
-
-function enterkey(e)
-{
+function enterkey(e) {
   // console.log(e.which);
-  if(e.which === 13)
-  onSubmit();
+  if (e.which === 13) onSubmit();
 }
-
-
 
 function AddTask() {
-    return (
-        <div className = "add-task">
-        <h1>
-          Add Task
-        </h1>
-        <div className = "input-area">
-          <input type="text" id='taskMessage' onKeyPress={enterkey}/>
-          <button onClick = {onSubmit}>Add Task</button>
-        </div>
-        </div>
-    )
+  return (
+    <div className="add-task">
+      <h1>Add Task</h1>
+      <div className="input-area">
+        <input type="text" id="taskMessage" onKeyPress={enterkey} />
+        <button onClick={onSubmit}>Add Task</button>
+      </div>
+    </div>
+  );
 }
 
-export default AddTask
+export default AddTask;
--- src/components/ShowTasks/ShowTasks.jsx
+++ src/components/ShowTasks/ShowTasks.jsx
@@ -1,83 +1,84 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import './showtasks.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./showtasks.css";
 
 function ShowTasks() {
-    const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const serverurl = process.env.REACT_APP_SERVER_URL;
 
-    useEffect(() => {
-        axios.get('https://open-lake-assignment.herokuapp.com/tasks')
-        .then(res => setTasks(res.data))
-        .catch(err => console.log(err));
-    }, []);
+  useEffect(() => {
+    axios
+      .get(serverurl + "/tasks")
+      .then((res) => setTasks(res.data))
+      .catch((err) => console.log(err));
+  }, [serverurl]);
 
-    function checkedbox(value , id)
-    {
-        value = !value;
+  function checkedbox(value, id) {
+    value = !value;
 
-        const url = `https://open-lake-assignment.herokuapp.com/done/${id}`;
-        axios.put(url , {chk : value})
-        .then(res => {
-            // console.log(res.data);
-            window.location.reload();
-        })
-        .catch(err => console.log(err));   
+    const url = serverurl + `/done/${id}`;
+    axios
+      .put(url, { chk: value })
+      .then((res) => {
+        // console.log(res.data);
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
+  }
 
-    }
+  function deleteit(id) {
+    const url = serverurl + `/remove/${id}`;
+    axios
+      .delete(url)
+      .then((res) => {
+        // console.log(res.data);
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
+  }
 
-    function deleteit(id)
-    {
-        const url = `https://open-lake-assignment.herokuapp.com/remove/${id}`;
-        axios.delete(url)
-        .then(res => {
-            // console.log(res.data);
-            window.location.reload();
-        })
-        .catch(err => console.log(err));
-    }
+  function computedmessage(str) {
+    var res = "";
+    if (str.length <= 35) return str;
 
-    function computedmessage(str)
-    {
-        var res = "";
-        if(str.length <= 35)
-        return str;
-        
-        for(var i=0;i<32;i++)
-            res += str[i];
-        res += "...";
-        return res;
-    }
+    for (var i = 0; i < 32; i++) res += str[i];
+    res += "...";
+    return res;
+  }
 
-
-    return (
-        <div className = "show-tasks">
-        <h1>
-          Tasks
-        </h1>
-        {
-            tasks.map( tsk => (
-                <div className = "task-box" key={tsk.id} id={tsk.id} >
-                <h3>
-                <div class="tooltip">{computedmessage(tsk.task)}
-                    <span class="tooltiptext">{tsk.task}</span>
-                </div>
-                    {/*
+  return (
+    <div className="show-tasks">
+      <h1>
+        Tasks
+        {console.log(serverurl)}
+      </h1>
+      {tasks.map((tsk) => (
+        <div className="task-box" key={tsk.id} id={tsk.id}>
+          <h3>
+            <div class="tooltip">
+              {computedmessage(tsk.task)}
+              <span class="tooltiptext">{tsk.task}</span>
+            </div>
+            {/*
                     computedmessage(tsk.task)
                     // tsk.task + `${tsk.task.length}`
-                    */} </h3>
-                {
-                    tsk.done ?
-                    (<input type="checkbox" checked onChange={() => checkedbox(true , tsk.id)}/>)
-                    :
-                    (<input type="checkbox" onChange={() => checkedbox(false , tsk.id)}/>)
-                }
-                <button onClick = {() => deleteit(tsk.id)}>Delete</button>
-                </div>
-            ))
-        }
-        {/* {console.log(tasks)} */}
+                    */}{" "}
+          </h3>
+          {tsk.done ? (
+            <input
+              type="checkbox"
+              checked
+              onChange={() => checkedbox(true, tsk.id)}
+            />
+          ) : (
+            <input type="checkbox" onChange={() => checkedbox(false, tsk.id)} />
+          )}
+          <button onClick={() => deleteit(tsk.id)}>Delete</button>
         </div>
-    )
+      ))}
+      {/* {console.log(tasks)} */}
+    </div>
+  );
 }
 
-export default ShowTasks
+export default ShowTasks;
